6dd2d107c1f38ea47169fc9f8943b09d4eb09e0d
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2024-10-04 00:20:50 -0300

    fix: mudando estrutura do html

--- index.php
+++ index.php
@@ -4,78 +4,137 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap 5.3.3 Scroll Suave</title>
-
-    <!-- Bootstrap 5.3.3 CDN -->
+    <title>Apresentação - Ambiente Virtual Cliente/Servidor</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
 
-    <!-- Barra de navegação fixa -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top ml-3">
-        <a class="navbar-brand fs-2 ps-3" href="#"> Institito Federal Sul de Minas</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse fs-3" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#section1">Módulo</a>
+    <div id="sidebar">
+        <h2 class="text-center">Instalação</h2>
+        <nav class="nav flex-column">
+            <a class="nav-link" href="#passo0">Apresentação</a>
+            <a class="nav-link" href="#passo1">Passo 1: Configuração do Servidor</a>
+            <a class="nav-link" href="#passo2">Passo 2: Instalação do PHP</a>
+            <a class="nav-link" href="#passo4">Passo 4: Configuração do Ambiente</a>
+            <a class="nav-link" href="#passo5">Passo 5: Teste da Aplicação</a>
+        </nav>
+    </div>
+
+    <div class="content">
+        <section id="passo0">
+            <div class="pt-3">
+                <h1>Plataforma de Desenvolvimento em Software Livre e Servidores Web</h1>
+            </div>
+            <br>
+            <h3 class="text-center p-5">
+                <p>Essa unidade é dedicada ao desenvolvimento e entrega da Atividade Final. A proposta da atividade é elaborar um relatório sobre a implantação de um ambiente cliente/servidor para uma aplicação web (exemplo) ou criada por você. </p>
+            </h3>
+            <br />
+            <h2>Implantação de um ambiente virtual cliente/servidor</h2>
+            <ul class="text-start">
+                <li>
+                    <h4>Tarefa</h4>
+                    <p>
+                    <h4>Realizar a implantação de um ambiente virtual cliente/servidor de desenvolvimento para aplicações web.</h4>
+                    </p>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#section2">Seção 2</a>
+                <li>
+                    <h4>Objetivo</h4>
+                    <p>
+                    <h4>Aplicar os conhecimentos adquiridos na disciplina Plataforma de Desenvolvimento em Software Livre e Servidores Web do primeiro módulo da Pós-Graduação em Desenvolvimento Web por meio da construção do ambiente de trabalho.</h4>
+                    </p>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#section3">Seção 3</a>
+                <li>
+                    <h4>Forma de entrega</h4>
+                    <p>
+                    <h4>O estudante ou a equipe (máximo 03 alunos) deverá entregar um relatório com as informações abaixo sobre a atividade desenvolvida, ou seja, relatar como foi o processo de construção do ambiente virtual.</h4>
+                    </p>
                 </li>
             </ul>
-        </div>
-    </nav>
+        </section>
 
-    <!-- Seções -->
-    <section id="section1" class="bg-primary text-white text-center ">
-        <div class="pt-3">
-            <h1>Plataforma de Desenvolvimento em Software Livre e Servidores Web</h1>
-        </div>
-        <br>
-        <h3 class="text-center p-5">
-            <p>Essa unidade é dedicada ao desenvolvimento e entrega da Atividade Final. A proposta da atividade é elaborar um relatório sobre a implantação de um ambiente cliente/servidor para uma aplicação web (exemplo) ou criada por você. </p>
-        </h3>
-        <br/>
-        <h2>Implantação de um ambiente virtual cliente/servidor</h2>
-        <ul class="text-start">
-            <li>
-                <h4>Tarefa</h4>
-                <p>
-                    <h4>Realizar a implantação de um ambiente virtual cliente/servidor de desenvolvimento para aplicações web.</h4>
-                </p>
-            </li>
-            <li>
-                <h4>Objetivo</h4>
-                <p>
-                    <h4>Aplicar os conhecimentos adquiridos na disciplina Plataforma de Desenvolvimento em Software Livre e Servidores Web do primeiro módulo da Pós-Graduação em Desenvolvimento Web por meio da construção do ambiente de trabalho.</h4>
-                </p>
-            </li>
-            <li>
-                <h4>Forma de entrega</h4>
-                <p>
-                    <h4>O estudante ou a equipe (máximo 03 alunos) deverá entregar um relatório com as informações abaixo sobre a atividade desenvolvida, ou seja, relatar como foi o processo de construção do ambiente virtual.</h4>
-                </p>
-            </li>
-        </ul>
-    </section>
+        <section id="passo1">
+            <h2>Passo 1: Configuração do Servidor</h2>
+            <p>Nesta etapa, você irá configurar o servidor para garantir que ele esteja pronto para as instalações necessárias.</p>
 
-    <section id="section2" class="bg-secondary text-white text-center">
-        <h1>Seção 2</h1>
-    </section>
+            <div class="row">
+                <div class="col-md mb-4">
+                    <div class="image-container">
+                        <img src="/img/install-debian/install-debian1.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-debian/install-debian2.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
 
-    <section id="section3" class="bg-success text-white text-center">
-        <h1>Seção 3</h1>
-    </section>
 
-    <!-- Bootstrap JS -->
+                    <div class="image-container">
+                        <img src="/img/install-debian/install-debian3.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-debian/install-debian4.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
+
+                    <div class="image-container">
+                        <img src="/img/install-debian/install-debian5.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-debian/install-debian6.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
+
+                    <div class="image-container">
+                        <img src="/img/install-debian/install-debian7.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-debian/install-debian8.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+
+        <section id="passo2">
+            <h2>Passo 2: Instalação do PHP</h2>
+            <p>Execute os comandos necessários para instalar a versão mais recente do PHP no seu servidor.</p>
+
+            <div class="row">
+                <div class="col-md mb-4">
+                    <div class="image-container">
+                        <img src="/img/install-php-apache/update.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-php-apache/update-root.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
+
+
+                    <div class="image-container">
+                        <img src="/img/install-php-apache/install-apache.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-php-apache/apache-status-service.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
+
+                    <div class="image-container">
+                        <img src="/img/install-php-apache/doc-apache.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-php-apache/install-php.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
+
+                    <div class="image-container">
+                        <img src="/img/install-php-apache/php-version.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-php-apache/php-set-timezone.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
+
+                    <div class="image-container">
+                        <img src="/img/install-php-apache/php-create-info.jpeg" alt="texto" class="img-fluid rounded">
+                        <img src="/img/install-php-apache/php-instalado.jpeg" alt="texto" class="img-fluid rounded">
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+
+        <section id="passo4">
+            <h2>Passo 4: Configuração do Ambiente</h2>
+            <p>Configure o ambiente virtual para garantir que todas as dependências estejam corretas.</p>
+        </section>
+
+        <section id="passo5">
+            <h2>Passo 5: Teste da Aplicação</h2>
+            <p>Teste a aplicação para garantir que tudo está funcionando conforme esperado.</p>
+        </section>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
